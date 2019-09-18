b5c51b953ba3a14aa97b0a67fed6226d60ed0e46
Gabriel <gabriel.arcos.firebase@gmail.com>
2019-09-18 18:34:38 -0300

    primero proyecto angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Componente0Component } from './componente0/componente0.component';
+import { Componente1Component } from './componente1/componente1.component';
+import { Componente2Component } from './componente2/componente2.component';
+import { ComponenteDetalleComponent } from './componente-detalle/componente-detalle.component';
+import { Routes,RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: Componente0Component},
+  {path: 'componente1', component: Componente1Component},
+  {path: 'componente2', component: Componente2Component},
+  //url con parametros
+  {path: 'componente2/:id', component: Componente2Component},
+  {path: 'componente1/:id/:titulo', component: Componente1Component},
+  //esto hace que si la ruta no esta definida lo redirige
+  {path: '**', component: Componente0Component}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente0Component,
+    Componente1Component,
+    Componente2Component,
+    ComponenteDetalleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
